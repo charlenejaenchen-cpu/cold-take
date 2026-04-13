8a22b5d141c00f4c1c892472a0c368e60f8e2d70
Charlene Jänchen <charlene.jaenchen@code.berlin>
2026-04-13 22:03:17 +0200

    feat: render most viewed takes on home page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 // --- DEPENDENCIES ---
 import express from 'express';
+import Article from '../models/Article.js';
+
 const router = express.Router();
 
 
 // --- ROUTES ---
 
 // GET Home
-router.get('/', (req, res) =>{
-    res.render('index');
+router.get('/', async (req, res) => {
+    const article = await Article.find().sort({ viewCount: -1}).limit(10);
+    res.render('index', { article });
 });
 
 export default router;
