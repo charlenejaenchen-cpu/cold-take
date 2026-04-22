8665e1e364f8dbed25eb511f2d7b7a4ce9fd3b18
Charlene Jänchen <charlene.jaenchen@code.berlin>
2026-04-22 18:19:57 +0200

    add responsive media queries for tablet (992px) and mobile (768px)

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // --- ROUTES ---
 
-// GET Home
+// GET Landingpage
 router.get('/', async (req, res) => {
     const article = await Article.find().sort({ viewCount: -1}).limit(10);
     res.render('index', { article });
